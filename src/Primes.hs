55839c7f3612c77799538e0d194016423e99afea
egregors <root@egregors.com>
2020-01-21 15:47:57 +1000

    primes and tests

---
+++ src/Primes.hs
@@ -0,0 +1,17 @@
+module Primes where
+
+primes :: [Int]
+primes = sieve [2 .. 10000]
+
+sieve :: [Int] -> [Int]
+sieve []                 = []
+sieve (nextPrime : rest) = nextPrime : sieve noFactors
+  where noFactors = filter ((/= 0) . (`mod` nextPrime)) rest
+
+isPrime :: Int -> Maybe Bool
+isPrime n | n < 2              = Nothing
+          | n >= length primes = Nothing
+          | otherwise          = Just (n `elem` primes)
+
+primeFactors :: Int -> Maybe [Int]
+primeFactors = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,28 @@
+import           Test.QuickCheck
+import           Primes
+import           Data.Maybe
+
+prop_validPrimesOnly val = if val < 2 || val >= length primes
+  then isNothing result
+  else isJust result
+  where result = isPrime val
+
+prop_primesArePrime val = if result == Just True
+  then length divisors == 0
+  else True
+ where
+  result   = isPrime val
+  divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+
+prop_nonPrimesAreComposite val = if result == Just False
+  then length divisors > 0
+  else True
+ where
+  result   = isPrime val
+  divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck prop_validPrimesOnly
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_primesArePrime
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_nonPrimesAreComposite
